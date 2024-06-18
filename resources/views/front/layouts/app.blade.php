5c16c95f52f4103d4a17e000e5bf6d7d25d6f8cc
CodingWithAbdou <coder.abdou@gmail.com>
2024-06-18 18:21:36 +0100

    - change call library

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -23,7 +23,8 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/slick/slick.css') }}" />
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/lib/slick/slick-theme.css') }}" />
     {{-- Library Icons --}}
-    <link href="{{ asset('../node_modules/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/dist/boxicons.js' rel='stylesheet'>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
         integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -67,7 +68,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
         integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    {{-- <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script> --}}
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
 
     @vite('resources/js/app.js')
 
